bff5a849891050d19d6ba7c3b6405a2cfc06425a
Borja Molina Catalan <bomoca@floridauniversitaria.es>
2016-09-28 13:11:15 +0200

    first commit

---
+++ ia.php
@@ -0,0 +1,21 @@
+<?php
+
+Class ia 
+{
+//Atributos
+public $columnas=2;
+public $fila=3;
+
+
+//Método
+public function getFila(){
+return $this->fila;
+
+}
+
+public function getColumna(){
+return $this->columnas;
+
+}
+
+}
